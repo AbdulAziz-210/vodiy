84ce8afd6cbafab3380effc23b76b7083f2234a7
AbdulAziz-210 <lexustoyota2002@gmail.com>
2024-08-21 15:12:32 +0500

    added credit, gadget, interest sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $(".credit").slick({
     dots: true,
-    prevArrow: "<button>helo</button>"
+    nextArrow: `<button class="slick-next slick-arrow"><i class="fa-solid fa-chevron-right"></i></button>`,
+    prevArrow: `<button class="slick-prev slick-arrow"><i class="fa-solid fa-chevron-left"></i></button>`
   });
 });
+
+// $(document).ready(function () {
+//   $(".scrolling-section").slick({
+//     dots: false
+//   });
+// });
