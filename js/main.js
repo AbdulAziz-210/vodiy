946f8dcf031781fa950031e72a3404e94b0e277b
AbdulAziz-210 <lexustoyota2002@gmail.com>
2024-08-26 15:41:16 +0500

    added slick carusels

--- js/main.js
+++ js/main.js
@@ -6,13 +6,18 @@ $(document).ready(function () {
   });
 });
 
-// $(document).ready(function () {
-//   $(".scrolling-section").slick({
-//     dots: false
-//   });
-// });
+//
+$(document).ready(function () {
+  $(".maybe-list").slick({
+    // dots: true,
+    slidesToShow: 6,
+    infinite: true,
+    nextArrow: `<button class="slick-next slick-arrow arrow-maybe arrow-maybe-next"><i class="fa-solid fa-chevron-right"></i></button>`,
+    prevArrow: `<button class="slick-prev slick-arrow arrow-maybe arrow-maybe-prev"><i class="fa-solid fa-chevron-left"></i></button>`
+  });
+});
 
-const item = document.querySelector(".go-to-item");
-const img = document.querySelector(".go-to-img");
+// const item = document.querySelector(".go-to-item");
+// const img = document.querySelector(".go-to-img");
 
-function first() {}
+// function first() {}
